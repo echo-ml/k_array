138f11b205344010f1ad692422d7a8a7a689d841
Ribulous Barnulous <ribulous.barnulous@gmail.com>
2014-12-03 09:15:40 -0500

    building out shape functionality

--- include/echo/k_array/shape.h
+++ include/echo/k_array/shape.h
@@ -12,7 +12,17 @@ namespace echo { namespace k_array {
 ///////////////
 
 struct Dimension {
-  static constexpr StaticIndex<-1> Dynamic{};
+  static const IndexInteger kDynamic = -1;
+  using Dynamic = StaticIndex<kDynamic>;
+};
+
+////////////
+// Stride //
+////////////
+
+struct Stride {
+  static const IndexInteger kDynamic = -1;
+  using Dynamic = StaticIndex<kDynamic>;
 };
 
 ///////////////////////
@@ -26,7 +36,7 @@ TICK_TRAIT(is_dimensionality) {
         const_algorithm::and_(
           fatal::constant_sequence<
               bool
-            , (Values > 0) || (Values == Dimension::Dynamic)...
+            , (Values > 0) || (Values == Dimension::Dynamic())...
           >()
         )
      >
@@ -45,11 +55,11 @@ TICK_TRAIT(
 {
 };
 
-//////////////
-// is_shape //
-//////////////
+/////////////////
+// is_subshape //
+/////////////////
 
-TICK_TRAIT(is_shape) {
+TICK_TRAIT(is_subshape) {
   template<class Shape>
   auto requires_(const Shape& shape) -> tick::valid<
       is_true<is_dimensionality<typename Shape::Dimensionality>>
@@ -59,6 +69,64 @@ TICK_TRAIT(is_shape) {
     , is_true_c<Shape::Dimensionality::size == Shape::Strides::size>
   >;
 };
+
+/////////////////////////
+// is_contiguous_shape //
+/////////////////////////
+
+TICK_TRAIT(is_contiguous_shape) {
+  template<class Shape>
+  auto requires_(const Shape& shape) -> tick::valid<
+      is_true<is_dimensionality<typename Shape::Dimensionality>>
+    , decltype(shape.template dynamic_extent<0>())
+    , is_false<is_subshape<Shape>>
+  >;
+};
+
+//////////////
+// is_shape //
+//////////////
+
+TICK_TRAIT(is_shape) {
+  template<class Shape>
+  auto requires_(const Shape& shape) -> tick::valid<
+      is_true_c<is_contiguous_shape<Shape>() || is_subshape<Shape>()>
+  >;
+};
+
+/////////////////////
+// is_static_shape //
+/////////////////////
+
+TICK_TRAIT(
+    is_static_shape
+  , is_shape<_>
+) 
+{
+  template<class Shape>
+  auto requires_(const Shape& shape) -> tick::valid<
+      is_false_c<
+          const_algorithm::contains(
+              typename Shape::Dimensionality()
+            , Dimension::Dynamic()
+          )
+      >
+  >;
+};
+
+////////////////////
+// Dimensionality //
+////////////////////
+
+template<class Shape>
+using Dimensionality = typename Shape::Dimensionality;
+
+////////////////////
+// StrideSequence //
+////////////////////
+
+template<class Shape>
+using StrideSequence = typename Shape::Strides;
 
 //////////////
 // get_rank //
@@ -72,17 +140,173 @@ constexpr IndexInteger get_rank() {
   return Shape::Dimensionality::size;
 }
 
+//////////////////////
+// is_static_extent //
+//////////////////////
+
+template<
+    int I
+  , class Shape
+  , enable_if<is_shape<Shape>> = 0
+>
+constexpr bool is_static_extent() {
+  return get<I>(Dimensionality<Shape>()) != Dimension::kDynamic;
+}
+
 ////////////////
 // get_extent //
 ////////////////
+
+template<
+    int I
+  , class Shape
+  , enable_if<is_shape<Shape>> = 0
+  , enable_if_c<is_static_extent<I, Shape>()> = 0
+>
+constexpr auto 
+get_extent(const Shape& shape) ->
+  decltype(get<I>(Dimensionality<Shape>()))  
+{
+  return {};
+}
+
+template<
+    int I
+  , class Shape
+  , enable_if<is_shape<Shape>> = 0
+  , disable_if_c<is_static_extent<I, Shape>()> = 0
+>
+IndexInteger get_extent(const Shape& shape) {
+  using namespace const_algorithm;
+  return shape.template dynamic_extent<
+      count(left<I>(Dimensionality<Shape>()), Dimension::Dynamic())
+  >();
+}
+
+//////////////////////
+// is_static_stride //
+//////////////////////
+
+template<
+    int I
+  , class Shape
+  , enable_if<is_contiguous_shape<Shape>> = 0
+>
+constexpr bool is_static_stride() {
+  using namespace const_algorithm;
+  return !contains(left<I>(Dimensionality<Shape>()), Dimension::Dynamic());
+}
+
+template<
+    int I
+  , class Shape
+  , enable_if<is_subshape<Shape>> = 0
+>
+constexpr bool is_static_stride() {
+  return get<I>(StrideSequence<Shape>()) != Stride::kDynamic;
+}
 
 ////////////////
 // get_stride //
 ////////////////
 
+template<
+    int I
+  , class Shape
+  , enable_if<is_contiguous_shape<Shape>> = 0
+  , enable_if_c<is_static_stride<I, Shape>()> = 0
+>
+constexpr auto get_stride(const Shape& shape) ->
+    StaticIndex<
+        const_algorithm::product(
+            const_algorithm::left<I>(Dimensionality<Shape>())
+        )
+    >
+{
+  return {};
+}
+
+template<
+    int I
+  , class Shape
+  , enable_if<is_contiguous_shape<Shape>> = 0
+  , disable_if_c<is_static_stride<I, Shape>()> = 0
+>
+constexpr IndexInteger get_stride(const Shape& shape) {
+  return get_extent<I-1>(shape) * get_stride<I-1>(shape);
+}
+
+template<
+    int I
+  , class Shape
+  , enable_if<is_subshape<Shape>> = 0
+  , enable_if_c<is_static_stride<I, Shape>()> = 0
+>
+constexpr auto get_stride(const Shape& shape) ->
+    decltype(get<I>(StrideSequence<Shape>()))
+{
+  return {};
+}
+
+template<
+    int I
+  , class Shape
+  , enable_if<is_subshape<Shape>> = 0
+  , disable_if_c<is_static_stride<I, Shape>()> = 0
+>
+IndexInteger get_stride(const Shape& shape) {
+  using namespace const_algorithm;
+  return shape.template dynamic_stride<
+      count(left<I>(StrideSequence<Shape>()), Stride::Dynamic())
+  >();
+}
+
 //////////////////////
 // get_num_elements //
 //////////////////////
+
+template<
+    class Shape
+  , enable_if<is_static_shape<Shape>> = 0
+>
+constexpr auto get_num_elements(const Shape& shape) ->
+    StaticIndex<
+        const_algorithm::product(Dimensionality<Shape>())
+    >
+{
+  return {};
+}
+
+namespace detail {
+
+template<
+    int I
+  , class Shape
+  , enable_if_c<I == 0> = 0
+>
+IndexInteger get_num_elements_impl(const Shape& shape) {
+  return get_extent<I>(shape);
+}
+
+template<
+    int I
+  , class Shape
+  , enable_if_c<I != 0> = 0
+>
+IndexInteger get_num_elements_impl(const Shape& shape) {
+  return get_extent<I>(shape)*get_num_elements_impl<I-1>(shape);
+}
+
+} // end namespace detail
+
+template<
+    class Shape
+  , enable_if<is_shape<Shape>> = 0
+  , disable_if<is_static_shape<Shape>> = 0
+>
+IndexInteger get_num_elements(const Shape& shape) {
+  return detail::get_num_elements_impl<get_rank<Shape>()-1>(shape);
+}
 
 //////////////////
 // get_1d_index //
--- unittest/shape_test.cpp
+++ unittest/shape_test.cpp
@@ -1,5 +1,7 @@
 #include <echo/k_array/shape.h>
+#include <echo/test.h>
 #include <catch.hpp>
+#include <cassert>
 
 using namespace echo;
 using namespace echo::k_array;
@@ -24,22 +26,120 @@ struct Shape2 {
   IndexInteger dynamic_stride() const { return 1; }
 };
 
-TEST_CASE("shape tests") {
+struct Shape3 {
+  using Dimensionality = StaticIndex<3, 2, Dimension::kDynamic, Dimension::kDynamic>;
+
+  template<int I>
+  IndexInteger dynamic_extent() const { 
+    switch(I) {
+      case 0:
+        return 7;
+      case 1:
+        return 8;
+      default:
+        assert(0);
+    }
+  }
+};
+
+struct Subshape1 {
+  using Dimensionality = StaticIndex<2, 3, 4, Dimension::kDynamic>;
+  using Strides        = StaticIndex<2, 5, Stride::kDynamic, Stride::kDynamic>;
+  template<int I>
+  IndexInteger dynamic_extent() const { return 1; }
+
+  template<int I>
+  IndexInteger dynamic_stride() const { 
+    switch(I) {
+      case 0:
+        return 12;
+      case 1:
+        return 13;
+      default:
+        assert(0);
+    }
+  }
+};
+
+TEST_CASE("shape") {
+  Shape1 shape1;
+  Shape3 shape3;
+  Subshape1 subshape1;
+
   SECTION("dimensionality concept") {
-    REQUIRE(is_dimensionality<StaticIndex<3, Dimension::Dynamic>>());
-    REQUIRE(!is_dimensionality<StaticIndex<-3, Dimension::Dynamic>>());
+    REQUIRE(is_dimensionality<StaticIndex<3, Dimension::kDynamic>>());
+    REQUIRE(!is_dimensionality<StaticIndex<-3, Dimension::kDynamic>>());
     REQUIRE(!is_dimensionality<StaticIndex<>>());
     REQUIRE(!is_dimensionality<int>());
   }
   SECTION("stride_sequence concept") {
-    REQUIRE(is_stride_sequence<StaticIndex<3, Dimension::Dynamic>>());
-    REQUIRE(!is_stride_sequence<StaticIndex<-3, Dimension::Dynamic>>());
+    REQUIRE(is_stride_sequence<StaticIndex<3, Dimension::kDynamic>>());
+    REQUIRE(!is_stride_sequence<StaticIndex<-3, Dimension::kDynamic>>());
     REQUIRE(!is_stride_sequence<StaticIndex<>>());
     REQUIRE(!is_stride_sequence<int>());
   }
   SECTION("shape concept") {
+    REQUIRE(is_subshape<Shape1>());
+    REQUIRE(!is_subshape<Shape2>());
+    REQUIRE(!is_subshape<int>());
+
+    REQUIRE(is_contiguous_shape<Shape3>());
+
+    REQUIRE(is_shape<Shape3>());
     REQUIRE(is_shape<Shape1>());
-    REQUIRE(!is_shape<Shape2>());
-    REQUIRE(!is_shape<int>());
+
+    REQUIRE(is_static_shape<Shape1>());
+    REQUIRE(!is_static_shape<Shape3>());
+  }
+  SECTION("get_extent") {
+    type_equal<
+        decltype(get_extent<0>(shape3))
+      , StaticIndex<3>
+    >();
+    type_equal<
+        decltype(get_extent<1>(shape3))
+      , StaticIndex<2>
+    >();
+    REQUIRE(get_extent<2>(shape3) == 7);
+    REQUIRE(get_extent<3>(shape3) == 8);
+  }
+  SECTION("is_static_stride") {
+    REQUIRE(is_static_stride<0, Shape3>());
+    REQUIRE(is_static_stride<1, Shape3>());
+    REQUIRE(is_static_stride<2, Shape3>());
+    REQUIRE(!is_static_stride<3, Shape3>());
+
+    REQUIRE(is_static_stride<0, Subshape1>());
+    REQUIRE(!is_static_stride<2, Subshape1>());
+  }
+  SECTION("get_stride") {
+    type_equal<
+        decltype(get_stride<0>(shape3))
+      , StaticIndex<1>
+    >();
+    type_equal<
+        decltype(get_stride<1>(shape3))
+      , StaticIndex<3>
+    >();
+    type_equal<
+        decltype(get_stride<2>(shape3))
+      , StaticIndex<6>
+    >();
+    REQUIRE(get_stride<3>(shape3) == 42);
+
+    type_equal<
+        decltype(get_stride<0>(subshape1))
+      , StaticIndex<2>
+    >();
+    REQUIRE(get_stride<2>(subshape1) == 12);
+    REQUIRE(get_stride<3>(subshape1) == 13);
+  }
+
+  SECTION("get_num_elements") {
+    type_equal<
+        decltype(get_num_elements(shape1))
+      , StaticIndex<24>
+    >();
+    REQUIRE(get_num_elements(shape3) == 336);
   }
 }
