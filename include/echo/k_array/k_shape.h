9ad3e82ba63e63acad3abf12ac896acf5848206e
Ribulous Barnulous <ribulous.barnulous@gmail.com>
2014-12-12 08:38:11 -0500

    fixed ADL problems

--- include/echo/k_array/k_shape.h
+++ include/echo/k_array/k_shape.h
@@ -11,16 +11,43 @@ namespace echo { namespace k_array {
 // KShape //
 ////////////
 
+namespace detail {
+
+template<int N>
+class KShapeBase {
+ public:
+  template<
+      class... Extents
+  >
+  KShapeBase(Extents... extents) 
+    : _extents(extents...)
+  {
+  }
+
+  template<int I>
+  IndexInteger extent() const {
+    return get<I>(_extents);
+  }
+ private:
+  Index<N> _extents;
+};
+
+template<>
+class KShapeBase<0> {
+};
+
+} //end namespace detail
+
 template<IndexInteger... Dimensions>
 class KShape 
-  : Index<
+  : detail::KShapeBase<
         const_algorithm::count(StaticIndex<Dimensions...>(), Dimension::Dynamic())
-    >
+  >
 {
   static constexpr IndexInteger kNumDynamicExtents = 
               const_algorithm::count(StaticIndex<Dimensions...>(), Dimension::Dynamic());
 
-  using DynamicExtents = Index<kNumDynamicExtents>;
+  using Base = detail::KShapeBase<kNumDynamicExtents>;
 
  public:
   using Dimensionality = StaticIndex<Dimensions...>;
@@ -30,12 +57,12 @@ class KShape
     , enable_if<std::is_convertible<Extents, IndexInteger>...> = 0
   >
   KShape(Extents... dynamic_extents)
-    : DynamicExtents(dynamic_extents...)
+    : Base(dynamic_extents...)
   {}
 
   template<int I>
   IndexInteger dynamic_extent() const {
-    return get<I>(*static_cast<const DynamicExtents*>(this));
+    return Base::template extent<I>();
   }
 };
 
